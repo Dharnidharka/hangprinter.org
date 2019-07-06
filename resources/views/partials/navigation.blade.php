d2e815e3952c3dbd66fce9c356146a9cc17bf367
Torbjørn Ludvigsen <tobben@fastmail.fm>
2019-07-06 04:45:57 +0000

    Updated remote

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,17 +1,17 @@
 <div class="masthead clearfix">
   <div class="inner">
     <h3 class="masthead-brand">
-      <a href="../" style="text-decoration: none; color: #000;">
+      <a href="http://hangprinter.org" style="text-decoration: none; color: #000;">
         <img src="images/logo1.png" style="height: 50px; width: 50px; margin-top: -15px; margin-right: 10px;" />Hangprinter
       </a>
     </h3>
     <nav>
       <ul class="nav masthead-nav">
-        <li id="nav_documentation"><a href="documentation">Documentation</a></li>
-        <li id="nav_contribute"><a href="contribute">Contribute</a></li>
-        <li id="nav_contact"><a href="contact">Contact</a></li>
-        <li id="nav_about"><a href="about">About</a></li>
-        <li id="nav_blog"><a href="show_blog">Blog</a></li>
+        <li id="nav_documentation"><a href="http://hangprinter.org/doc/v3">Documentation</a></li>
+        <li id="nav_contribute"><a href="http://hangprinter.org/contribute">Contribute</a></li>
+        <li id="nav_contact"><a href="http://hangprinter.org/contact">Contact</a></li>
+        <li id="nav_about"><a href="http://hangprinter.org/about">About</a></li>
+        <li id="nav_blog"><a href="http://hangprinter.org/show_blog">Blog</a></li>
       </ul>
     </nav>
   </div>
